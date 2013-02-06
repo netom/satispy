6e86fdbb8b30fb39e1be3e0ec1d4dd40fe0cca6d
Fábián Tamás László <tamas.fabian@vengit.com>
2013-02-06 10:57:30 +0100

    Fixed description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(
     name='satispy',
     version='1.0a1',
-    description='An interface to SAT solver tools (like minsat)',
+    description='An interface to SAT solver tools (like minisat)',
     author='FÁBIÁN Tamás László',
     author_email='giganetom@gmail.com',
     url='https://github.com/netom/satispy',
