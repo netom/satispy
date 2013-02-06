7fc97aa1e5b2ab50fac5413a25911a0bc80b1308
Fábián Tamás László <tamas.fabian@vengit.com>
2013-02-06 11:26:39 +0100

    Tweaked setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     description='An interface to SAT solver tools (like minisat)',
     author='FÁBIÁN Tamás László',
     author_email='giganetom@gmail.com',
-    url='https://github.com/netom/satispy',
+    url='https://github.com/netom/satispy/',
+    download_url='https://github.com/netom/satispy/tarball/1_0a1',
+    license='BSD License',
+    platforms='OS Independent',
     packages=['satispy', 'satispy.io', 'satispy.solver'],
     classifiers=[
         'Development Status :: 3 - Alpha',
