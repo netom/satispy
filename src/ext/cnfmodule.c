875ef6c590d8b77243d8ef89bd5cbfe6df2a58da
Fábián Tamás László <tamas.fabian@vengit.com>
2013-02-07 21:40:41 +0100

    Imports working

--- src/ext/cnfmodule.c
+++ src/ext/cnfmodule.c
@@ -1,35 +1,94 @@
 #include <Python.h>
 
-static PyObject *CnfError;
+typedef struct {
+    PyObject_HEAD
+    /* Type-specific fields go here. */
+} cnf_VariableObject;
 
-static PyObject * cnf_system(PyObject *self, PyObject *args)
-{
-    const char *command;
-    int sts;
-
-    if (!PyArg_ParseTuple(args, "s", &command)) {
-        return NULL;
-    }
-
-    sts = system(command);
-    return Py_BuildValue("i", sts);
-}
-
-static PyMethodDef CnfMethods[] = {
-    {"system",  cnf_system, METH_VARARGS,
-     "Execute a shell command."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+static PyTypeObject cnf_VariableType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "cnf.Variable",            /*tp_name*/
+    sizeof(cnf_VariableObject),/*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    0,                         /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+    "Variable",                /* tp_doc */
 };
 
-PyMODINIT_FUNC initcnf(void)
+typedef struct {
+    PyObject_HEAD
+    /* Type-specific fields go here. */
+} cnf_CnfObject;
+
+static PyTypeObject cnf_CnfType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "cnf.Cnf",                 /*tp_name*/
+    sizeof(cnf_CnfObject),     /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    0,                         /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+    "Cnf expression",          /* tp_doc */
+};
+
+static PyMethodDef cnf_methods[] = {
+    {NULL}  /* Sentinel */
+};
+
+#ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
+#define PyMODINIT_FUNC void
+#endif
+
+PyMODINIT_FUNC initcnf(void) 
 {
-    PyObject *m;
+    PyObject* m;
 
-    m = Py_InitModule("cnf", CnfMethods);
-    if (m == NULL)
+    cnf_VariableType.tp_new = PyType_GenericNew;
+    cnf_CnfType.tp_new = PyType_GenericNew;
+
+    if (PyType_Ready(&cnf_VariableType) < 0) {
         return;
+    }
 
-    CnfError = PyErr_NewException("cnf.error", NULL, NULL);
-    Py_INCREF(CnfError);
-    PyModule_AddObject(m, "error", CnfError);
+    if (PyType_Ready(&cnf_CnfType) < 0) {
+        return;
+    }
+
+    m = Py_InitModule3("cnf", cnf_methods,
+                       "A mosule that provides fast CNF expression building tools");
+
+    Py_INCREF(&cnf_VariableType);
+    Py_INCREF(&cnf_CnfType);
+
+    PyModule_AddObject(m, "Variable", (PyObject *)&cnf_CnfType);
+    PyModule_AddObject(m, "Cnf", (PyObject *)&cnf_CnfType);
 }
