94812ce82a5348451233ebe4def4ac6c4cc83a53
Fábián Tamás László <tamas.fabian@vengit.com>
2013-02-07 08:37:11 +0100

    Experimental bit cnf

---
+++ hamiltonian_cycle_bit.py
@@ -0,0 +1,105 @@
+# Naive implementation of Hamilton-cycle searching in a graph
+#
+# Based on the following paper:
+#
+# Formalizing Dangerous SAT Encodings
+# Alexander Hertel, Philipp Hertel, and Alasdair Urquhart
+# Department of Computer Science,
+# University of Toronto, Toronto ON M5S 3G4, Canada,
+# [ahertel][philipp][urquhart]@cs.toronto.edu
+
+from satispy import BitVariable, BitCnf
+from satispy.solver import Minisat
+
+import networkx as nx
+
+N = 8
+M = 8
+
+g = nx.grid_2d_graph(N, M)
+
+lennodes = len(g.nodes())
+gnodes = g.nodes()
+
+exp = BitCnf((N*M)**2)
+
+# Variables
+# We create a variable for every node that
+# marks the position of that node in the path
+print "Creating variables..."
+varz = {}
+for n in gnodes:
+    varz[n] = []
+    for i in xrange(lennodes):
+        # n, m, position
+        varz[n].append(BitVariable("%d_%d_%d" % (n[0], n[1], i), (N*M)**2))
+
+# Total (X)
+print "Creating total clauses..."
+for i in gnodes:
+    c = BitCnf((N*M)**2)
+    for j in xrange(lennodes):
+        c |= varz[i][j]
+    exp &= c
+
+# Onto
+print "Creating onto clauses..."
+for j in xrange(lennodes):
+    c = BitCnf((N*M)**2)
+    for i in gnodes:
+        c |= varz[i][j]
+    exp &= c
+
+# 1-1 (X)
+print "Creating 1-1 calues..."
+for j in xrange(lennodes):
+    print j
+    for i1 in gnodes:
+        for i2 in gnodes:
+            if i1 != i2:
+                exp &= -varz[i1][j] | -varz[i2][j]
+
+# Fn
+print "Creating Fn calues..."
+for i in xrange(lennodes):
+    print i
+    for j1 in gnodes:
+        for j2 in gnodes:
+            if i1 != i2:
+                exp &= -varz[i][j1] | -varz[i][j2]
+
+# Edge
+print "Adding edge clauses..."
+for j in xrange(lennodes):
+    print j
+    for i in gnodes:
+        for k in gnodes:
+            if i != k and k not in g.neighbors(i):
+                exp &= -varz[i][j] | -varz[k][(j+1) % lennodes]
+            
+# Enabling minisat to write to stdout
+solver = Minisat('minisat %s %s')
+
+exit()
+
+print "Solving..."
+solution = solver.solve(exp)
+
+if not solution.success:
+    print "There is no Hamilton cycle in the graph."
+    exit()
+
+print "Extracting solution..."
+path = []
+for n in g.nodes():
+    nodepos = -1
+    for j in xrange(lennodes):
+        if solution[varz[n][j]]:
+            nodepos = j
+            break
+    path.append((nodepos, n))
+
+path.sort()
+
+for n in path:
+    print n[0], n[1]
--- satispy/cnf.py
+++ satispy/cnf.py
@@ -231,7 +231,6 @@ class Cnf(object):
     def __hash__(self):
         return hash(self.dis)
 
-
 class CnfFromString(Cnf):
     def __init__(self):
         pass
@@ -356,3 +355,133 @@ class CnfFromString(Cnf):
 
 # Change this to NaiveCnf if you want.
 cnfClass = Cnf
+
+class BitVariable(object):
+    varmap = {}
+    def __init__(self, name, varz):
+        if BitVariable.varmap.has_key(name):
+            self.varno = BitVariable.varmap[name]
+        else:
+            self.varno = len(BitVariable.varmap)
+            BitVariable.varmap[name] = self.varno
+        self.name = name
+        self.inverted = 0
+        self.chunk = self.varno / 32
+        #print (((self.varno % 16) * 2) + self.inverted)
+        self.bitfield = (1 << (((self.varno % 31) * 2) + self.inverted))
+        self.varz = varz
+
+    def __neg__(self):
+        v = BitVariable(self.name, self.varz)
+        v.inverted = 1 - self.inverted
+        v.bitfield = (1 << (((v.varno % 31) * 2) + v.inverted))
+        return v
+
+    def __and__(self, other):
+        c = BitCnf.create_from(self, self.varz)
+        return c & other
+
+    def __or__(self, other):
+        c = BitCnf.create_from(self, self.varz)
+        return c | other
+
+    def __xor__(self, other):
+        c = BitCnf.create_from(self, self.varz)
+        return c ^ other
+
+    def __rshift__(self, other): # implies
+        c = BitCnf.create_from(self, self.varz)
+        return -c | other
+
+    def __str__(self):
+        return ("-" if self.inverted else "") + self.name
+
+    def __eq__(self, other):
+        return self.name == other.name and self.inverted == other.inverted
+
+    def __hash__(self):
+        return hash(self.name) ^ hash(self.inverted)
+
+    def __cmp__(self, other):
+        if self == other:
+            return 0
+        if (self.name, self.inverted) < (other.name, other.inverted):
+            return -1
+        else:
+            return 1
+
+class BitCnf(object):
+    def __init__(self, varz):
+        self.clauses = []   # Bit table
+        self.varz = varz    # Number of possible variables
+
+    @classmethod
+    def _new_clause(cls,varz):
+        return numpy.zeros(varz/32+1, dtype=numpy.int64)
+
+    def dedup_clauses(self):
+        """Eliminate double clauses
+        """
+        newclauses = []
+        for clause in self.clauses:
+            if clause not in newclauses:
+                newclauses.append(clause)
+        self.clauses = newclauses
+
+    def dedup_vars(self):
+        """Eliminates e | -e type of expressions
+        """
+        lsmask = int('10'*32,2)
+        rsmask = int('01'*32,2)
+
+        for clause in self.clauses:
+            numpy.vectorize(lambda a: a & ~(a & (a << 1) & lsmask) & ~(a & (a >> 1) & rsmask))(self.clauses)
+
+    @classmethod
+    def create_from(self,v,varz):
+        if isinstance(v, BitVariable):
+            cnf = BitCnf(varz)
+            c = BitCnf._new_clause(varz)
+            c[v.chunk] |= v.bitfield
+            cnf.clauses.append(c)
+            return cnf
+        else:
+            return v
+
+    def __iand__(self, other):
+        other = self.create_from(other, self.varz)
+
+        for clause in other.clauses:
+            self.clauses.append(clause[:])
+        return self
+
+    def __or__(self, other):
+        other = self.create_from(other, self.varz)
+        newcnf = BitCnf(self.varz)
+
+        if len(self.clauses) > 0 and len(other.clauses) > 0:
+            for c1, c2 in [(c1,c2) for c1 in self.clauses for c2 in other.clauses]:
+                newcnf.clauses.append(c1 | c2) # Numpy for the rescue
+        elif len(self.clauses) == 0:
+            for clause in other.clauses:
+                newcnf.clauses.append(clause[:])
+        else:
+            for clause in self.clauses:
+                newcnf.clauses.append(clause[:])
+
+        return newcnf
+
+    def __and__(self, other):
+        other = self.create_from(other, self.varz)
+        newcnf = BitCnf(self.varz)
+        for clause in self.claues:
+            newcnf.clauses.append(clause[:])
+        if isinstance(other, BitVariable):
+            c = BitCnf._new_clause(self.varz)
+            c[other.chunk] |= other.bitfield
+            newcnf.clauses.append(c)
+            return newcnf
+        else:
+            for clause in other.claues:
+                newcnf.clauses.append(clause[:])
+            return newcnf
