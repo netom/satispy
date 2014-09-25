0e3661123779a7976d7809a66220a1de360f1519
David Hoepelman <dhoepelman@gmail.com>
2014-09-25 16:51:24 +0200

    replace distutils with setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='satispy',
-    version='1.0a4',
+    version='1.0a5',
     description='An interface to SAT solver tools (like minisat)',
     author='FÁBIÁN Tamás László',
     author_email='giganetom@gmail.com',
