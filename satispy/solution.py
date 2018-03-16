22a68aa9c12d836ddcf4e30f051397eef90b4cf7
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2018-03-16 14:48:01 -0700

    bugfix: by default, Solution uses *global* dictionary for all variables, instead of having one dictionary per Solution instance. That global dictionary gets messed up when use runs the solver several times in one process.

--- satispy/solution.py
+++ satispy/solution.py
@@ -1,7 +1,9 @@
 class Solution(object):
-    def __init__(self, success=False, error=False, varmap={}):
+    def __init__(self, success=False, error=False, varmap=None):
         self.success = success
         self.error = error
+        if varmap is None:
+            varmap = {}
         self.varmap = varmap
 
     def __getitem__(self, i):
