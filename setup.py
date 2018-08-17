550de35d5cb2b00b03ac62e60b29f435f73b1cb2
Fábián Tamás László <giganetom@gmail.com>
2018-08-17 10:21:38 +0200

    Bump version and development status

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ from setuptools import setup
 
 setup(
     name='satispy',
-    version='1.0a6',
+    version='1.1b',
     description='An interface to SAT solver tools (like minisat)',
     author='FÁBIÁN Tamás László',
     author_email='giganetom@gmail.com',
     url='https://github.com/netom/satispy/',
-    download_url='https://github.com/netom/satispy/tarball/1.0a6#egg=satispy-1.0a6',
+    download_url='https://github.com/netom/satispy/tarball/1.1#egg=satispy-1.1',
     license='BSD License',
     platforms='OS Independent',
     packages=['satispy', 'satispy.io', 'satispy.solver'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
